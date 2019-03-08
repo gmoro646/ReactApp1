814bf86d603abfae2f8ff1160c129c9429d00606
gio <gio@macmoro.com>
2019-03-07 22:18:31 -0700

    Updates

---
+++ precache-manifest.5fb16270e6d6ea0d2fe15f1f128924a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a1eb04beeb9a14f87e6",
+    "url": "gmoro646.github.io/ReactAsg1/static/css/main.748d6835.chunk.css"
+  },
+  {
+    "revision": "5a1eb04beeb9a14f87e6",
+    "url": "gmoro646.github.io/ReactAsg1/static/js/main.5a1eb04b.chunk.js"
+  },
+  {
+    "revision": "255a57861ae4fb6987f0",
+    "url": "gmoro646.github.io/ReactAsg1/static/js/runtime~main.255a5786.js"
+  },
+  {
+    "revision": "8074d1e1a8194c9dc1bb",
+    "url": "gmoro646.github.io/ReactAsg1/static/css/2.0023dc67.chunk.css"
+  },
+  {
+    "revision": "8074d1e1a8194c9dc1bb",
+    "url": "gmoro646.github.io/ReactAsg1/static/js/2.8074d1e1.chunk.js"
+  },
+  {
+    "revision": "92b3c55df467a139000304bc1238d66d",
+    "url": "gmoro646.github.io/ReactAsg1/index.html"
+  }
+];
