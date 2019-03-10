61f60a9b44311345d70bc42cbf4c400e73ed1019
gio <gio@macmoro.com>
2019-03-10 14:15:58 -0600

    Updates

---
+++ precache-manifest.70598890ce3ea625d2ce38d0e0d6fac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed27256308c8c2500481",
+    "url": "/ReactApp1/static/css/main.748d6835.chunk.css"
+  },
+  {
+    "revision": "ed27256308c8c2500481",
+    "url": "/ReactApp1/static/js/main.ed272563.chunk.js"
+  },
+  {
+    "revision": "8bc9cf8336bf64a3f6f9",
+    "url": "/ReactApp1/static/js/runtime~main.8bc9cf83.js"
+  },
+  {
+    "revision": "8074d1e1a8194c9dc1bb",
+    "url": "/ReactApp1/static/css/2.0023dc67.chunk.css"
+  },
+  {
+    "revision": "8074d1e1a8194c9dc1bb",
+    "url": "/ReactApp1/static/js/2.8074d1e1.chunk.js"
+  },
+  {
+    "revision": "0a9b4a291eb6470431bdecd288d81da2",
+    "url": "/ReactApp1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "gmoro646.github.io/ReactApp1/precache-manifest.e206dc67655627b2cc678284a2e915be.js"
+  "/ReactApp1/precache-manifest.70598890ce3ea625d2ce38d0e0d6fac4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("gmoro646.github.io/ReactApp1/index.html", {
+workbox.routing.registerNavigationRoute("/ReactApp1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
